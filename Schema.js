1a6c896254f2fc0b2a17585edc676fb18fff4a26
tarun-tej-2007 <taruntej947@gmail.com>
2025-02-20 14:26:26 +0530

    created Schema

---
+++ Schema.js
@@ -0,0 +1,27 @@
+const moongose = require('mongoose');
+
+const AirplanesSchema = new moongose.Schema({
+    name : {
+        type : String,
+            required : true},
+    year : {
+        type : Number,
+        required : true
+    }
+
+    });
+
+const ReviewSchema = new moongose.Schema({
+    review:{
+        type : String,
+        required : true
+    },
+    rating : {
+        type : Number,
+        required : true
+    },
+    airplane : {
+        type : moongose.Schema.Types.ObjectId,
+        ref : 'Airplanes'
+    }
+})
